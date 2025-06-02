987dede2cc30b57855b2c8feab302bcce4c48db5
milzl <milzlars88@gmail.com>
2025-06-02 22:09:25 +0200

    generic vector

---
+++ src/vector.rs
@@ -0,0 +1,14 @@
+use std::ops::(Add, Mul, Sub)
+
+/// A generic N-dimensional vector.
+#[derive(Debug, Clone, Copy)]
+pub struct VecN<T, const N: usize> {
+    pub data[T; N],
+}
+
+impl<T: Copy, const N: usize> VecN<T, N> {
+    /// Creates a new VecN from an array of values.
+    pub fn new (data: [T,N]) -> Self {
+        Self { data }
+    }
+}
