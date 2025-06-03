38c0711509ebb00d4f5140a10225f92a63187547
milzl <milzlars88@gmail.com>
2025-06-03 21:13:38 +0200

    fixing build errors

--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,14 @@
-use std::ops::(Add, Mul, Sub)
+use std::ops::{Add, Mul, Sub};
 
 /// A generic N-dimensional vector.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct VecN<T, const N: usize> {
-    pub data[T; N],
+    pub data: [T; N],
 }
 
 impl<T: Copy, const N: usize> VecN<T, N> {
     /// Creates a new VecN from an array of values.
-    pub fn new (data: [T,N]) -> Self {
+    pub fn new (data: [T; N]) -> Self {
         Self { data }
     }
 }
