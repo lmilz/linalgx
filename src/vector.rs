997c6a0810b842c254c8fba0fe88cba4feb26b23
milzl <milzlars88@gmail.com>
2025-06-04 22:07:13 +0200

    format

--- src/vector.rs
+++ src/vector.rs
@@ -104,7 +104,7 @@ where
     pub fn cross(&self, other: &Self) -> Self {
         let [x1, y1, z1] = self.data;
         let [x2, y2, z2] = other.data;
-        Vec3::new([y1*z2 - z1*y2, z1*x2 - x1*z2, x1*y2 - y1*x2])
+        Vec3::new([y1 * z2 - z1 * y2, z1 * x2 - x1 * z2, x1 * y2 - y1 * x2])
     }
 }
 
