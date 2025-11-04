d8129d5cad388c732c38d7943986fd1abd71b025
milzl <milzlars88@gmail.com>
2025-11-04 20:36:53 +0100

    map function for matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,3 @@
-use num_traits::{Float, Zero};
-use std::ops::{Add, Mul, Sub};
-
 /// A generic MxN-dimensional matrix.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct MatMN<T, const M: usize, const N: usize> {
@@ -9,7 +6,31 @@ pub struct MatMN<T, const M: usize, const N: usize> {
 
 impl<T: Copy, const M: usize, const N: usize> MatMN<T, M, N> {
     /// Creates a new MatMN from an array of values.
-    pub fn new(data: [[T;M];N]) -> Self {
+    pub fn new(data: [[T; M]; N]) -> Self {
         Self { data }
+    }
+
+    /// Applies a function to each element of the matrix
+    pub fn map<U, F>(&self, f: F) -> MatMN<U, M, N>
+    where
+        F: Fn(T) -> U,
+        U: Copy,
+    {
+        MatMN::new(std::array::from_fn(|i| {
+            std::array::from_fn(|j| f(self.data[i][j]))
+        }))
+    }
+}
+
+/// Unit Test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_matmn_map() {
+        let mat = MatMN::new([[1, 2], [3, 4]]);
+        let mapped = mat.map(|x| x * 2);
+        assert_eq!(mapped, MatMN::new([[2, 4], [6, 8]]));
     }
 }
