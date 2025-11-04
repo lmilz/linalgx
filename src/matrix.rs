ad3251bf7e6c0835b1dc71e06978c8deb1210e96
milzl <milzlars88@gmail.com>
2025-11-04 19:42:13 +0100

    new function

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,15 @@
+use num_traits::{Float, Zero};
+use std::ops::{Add, Mul, Sub};
+
 /// A generic MxN-dimensional matrix.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct MatMN<T, const M: usize, const N: usize> {
     pub data: [[T; M]; N],
+}
+
+impl<T: Copy, const M: usize, const N: usize> MatMN<T, M, N> {
+    /// Creates a new MatMN from an array of values.
+    pub fn new(data: [[T;M];N]) -> Self {
+        Self { data }
+    }
 }
