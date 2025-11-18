547f5dc4b100aa5c5325de5f8e7e86fea3d83e65
lmilz <milzlars88@gmail.com>
2025-11-18 14:55:42 +0000

    zip_with function

--- src/matrix.rs
+++ src/matrix.rs
@@ -20,6 +20,18 @@ impl<T: Copy, const M: usize, const N: usize> MatMN<T, M, N> {
             std::array::from_fn(|j| f(self.data[i][j]))
         }))
     }
+
+    /// Combines two matrices element-wise using a given binary function
+    pub fn zip_with<U, R, F>(&self, other: MatMN<U, M, N>, f: F) -> MatMN<R, M, N>
+    where
+        F: Fn(T, U) -> R,
+        U: Copy,
+        R: Copy,
+    {
+        MatMN::new(std::array::from_fn(|i| {
+            std::array::from_fn(|j| f(self.data[i][j], other.data[i][j]))
+        }))
+    }
 }
 
 /// Unit Test
@@ -30,7 +42,15 @@ mod tests {
     #[test]
     fn test_matmn_map() {
         let mat = MatMN::new([[1, 2], [3, 4]]);
-        let mapped = mat.map(|x| x * 2);
-        assert_eq!(mapped, MatMN::new([[2, 4], [6, 8]]));
+        let doubled = mat.map(|x| x * 2);
+        assert_eq!(doubled, MatMN::new([[2, 4], [6, 8]]));
+    }
+
+    #[test]
+    fn test_matmn_zip_with() {
+        let mat1 = MatMN::new([[1, 2], [3, 4]]);
+        let mat2 = MatMN::new([[5, 6], [7, 8]]);
+        let result = mat1.zip_with(mat2, |a, b| a + b);
+        assert_eq!(result, MatMN::new([[6, 8], [10, 12]]));
     }
 }
